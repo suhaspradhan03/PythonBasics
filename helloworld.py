082e0c8b2cf320f33899957cde94bcbfdbd6fc63
suhaspradhan03 <87471309+suhaspradhan03@users.noreply.github.com>
2021-08-14 15:00:32 +0400

    Add files via upload
    This has details which are definitely more than HelloWorld

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+print('hello, World!!')             # Print - is a function
+print('whats going on su??')     # Everything within the () is called an argument
+print(6+5)          # For numbers in this IDE as of now the '' are not required
+print()             # This is to leave a line in the code
+print(7*5)          # Multiplying two numbers
+print(2534565/5)    # Dividing numbers
+print()             # leaving a line
+print("The end", "or is it??", "Learn Python properly", 77)
+# Can write anything in between this print statement and " , " is used to split the statement.
+# Anything inside The parenthesis () is called an argument, including numbers.
+# so there are 4 arguments in the above print(function) statement.
+print()
+print('Test in udemy')
+print(6*7)
+print('My hovercraft is full of eels')
+print()
+# This line becomes invalid (look at line 17)
+print("test")
+# Every time we need a blank line in the code we have to put print().
+print()
+print('date - 02/05/21 - May 02nd 2021')
+print('came back to python on --> 16th May 2021 [16/05/21] ')
+# came back on the [21/05/21]
